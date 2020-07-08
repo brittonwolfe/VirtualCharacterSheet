d95c2984e7329eb6867e28ad7597baac4d16d2c1
Britton <britton.wolfe1999@gmail.com>
2020-07-08 18:24:09 -0400

    started writing very basic GUI code to lay a foundation

--- core/ui.py
+++ core/ui.py
@@ -1,1 +1,12 @@
 from tkinter import Tk
+
+class GUI():
+	window = Tk()
+	components = []
+	def __init__(self):
+		pass
+	def append(self, component):
+		self.components.append(component)
+	def pack(self):
+		for component in self.components:
+			component.pack()
