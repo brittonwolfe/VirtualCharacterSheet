cdba84da65cb404989271afda048d911a550d224
Britton <britton.wolfe1999@gmail.com>
2020-07-11 01:25:36 -0400

    some implementation of a custom shell for the core_5e brew

--- core_5e/shell.py
+++ core_5e/shell.py
@@ -1,8 +1,23 @@
 from core.shell import PyTui, add_base, cmd_roll
 
+STAT_LIST = [ 'str', 'strength', 'dex', 'dexterity', 'con', 'constitution', 'int', 'intelligence', 'wis', 'wisdom', 'cha', 'charisma']
+
 def stat_check(args):
-	local.This.Character
+	stat = None
+	if args[0].lower() in STAT_LIST:
+		stat = args[0].lower()
+	if stat is None:
+		print('No valid stat provided')
+		return
+	pass #local.This.Character
+
+def versatile_roll(args):
+	if args[0].lower() in STAT_LIST:
+		return stat_check(args)
+	else:
+		return cmd_roll(args)
 
 character_tui = PyTui(add_base({
-	'check': stat_check
+	'check': stat_check,
+	'roll': versatile_roll
 }))
