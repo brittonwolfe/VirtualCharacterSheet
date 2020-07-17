bb9e4624271c1db5f4066135ac97456cf8e1c8e6
Britton <britton.wolfe1999@gmail.com>
2020-07-17 12:55:26 -0400

    added some documentation and shorthands to the core5e shell

--- core_5e/shell.py
+++ core_5e/shell.py
@@ -1,11 +1,19 @@
 from core.shell import PyTui, add_base, cmd_roll
 
 STAT_LIST = [ 'str', 'strength', 'dex', 'dexterity', 'con', 'constitution', 'int', 'intelligence', 'wis', 'wisdom', 'cha', 'charisma']
+SHORTHAND_MAP = { 'str': 'Strength', 'dex': 'Dexterity', 'con': 'Constitution', 'int': 'Intelligence', 'wis': 'Wisdom', 'cha': 'Charisma' }
 
 def stat_check(args):
+	'''stat [name]
+	todo doc
+	'''
 	stat = None
 	if args[0].lower() in STAT_LIST:
-		stat = args[0].lower().capitalize()
+		stat = args[0].lower()
+		if len(stat) == 3:
+			stat = SHORTHAND_MAP[stat]
+		else:
+			stat = stat.lower()
 	else:
 		print('No valid stat provided')
 		return
@@ -21,14 +29,19 @@ def stat_check(args):
 				print('too many times arguments given')
 				return
 			times = int(arg)
-	if len(stat) == 3:
-		stat = STAT_LIST[STAT_LIST.index(stat) + 1].capitalize()
 	output = 0
 	for _ in range(times or 1):
 		output += local.This.Character.DoBehavior(stat + 'Check')
 	return output
 
 def versatile_roll(args):
+	'''roll [stat] ...
+	a versatile wrapper around the base roll command
+	that allows the user to specify a stat to use for
+	the roll as well.
+
+	stat - the name of the stat to use
+	'''
 	if args[0].lower() in STAT_LIST:
 		return stat_check(args)
 	else:
