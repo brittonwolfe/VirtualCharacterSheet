00c51c4507df0b35ccfcc075f6db1273cf4075d0
Britton W <britton.wolfe1999@gmail.com>
2020-08-13 12:02:24 -0400

    converted the __config__ from a simple dict into a new Config object for easier implementation

--- core/config.py
+++ core/config.py
@@ -2,20 +2,26 @@ clr.AddReference('vcs')
 from configparser import ConfigParser
 from os.path import expanduser
 
-config = ConfigParser()
-config.read(expanduser('~/.config/vcs.conf'))
+class Config():
+	values = {}
+	parser = None
+	def __init__(self, path = '~/.config/vcs/config'):
+		self.parser = ConfigParser()
+		self.parser.read(expanduser(path))
+		for section in self.parser.sections():
+			obj = {}
+			for name in self.parser.items(section)[0]:
+				obj[name] = self.parser.get(section, name)
+			self.values[section] = obj
+	def has_section(self, name):
+		return hasattr(self.values, name)
+	def save(self):
+		for section in self.values:
+			for key in self.values[section]:
+				self.parser.set(section, key, str(self.values[section][key]))
+		self.parser.write()
+	def __getitem__(self, key):
+		return self.values[key]
 
 global __config__
-__config__ = {}
-
-for section in config.sections():
-	obj = {}
-	for name in config.items(section)[0]:
-		obj[name] = config.get(section, name)
-	__config__[section] = obj
-
-def save_config():
-	for section in __config__:
-		for key in __config__[section]:
-			config.set(section, key, str(__config__[section][key]))
-	config.write()
+__config__ = Config()
