91972fc53f76bba1db20a50a967a6bd3d3a30206
Britton W <britton.wolfe1999@gmail.com>
2020-08-13 17:14:56 -0400

    documentation and annotation

---
+++ core_5e/Pages/__init__.py
@@ -0,0 +1,7 @@
+#todo
+# The basic outline of how I want brew-API integration to work is:
+#	- the init script (this file) will add the views through a method
+#	- the method will copy the files to a working directory
+#	- the files will recieve hooks to the temporary file set to be deleted
+# This will all happen on brew load, and there will be a networking shell in
+# core/net.py that you can run from the main shell.
