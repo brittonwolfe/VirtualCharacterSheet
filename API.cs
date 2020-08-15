74390e00fcd00ec79a60fc36364b8e9a2261bdab
Britton W <britton.wolfe1999@gmail.com>
2020-08-14 22:47:49 -0400

    refactored stream disposal

--- API.cs
+++ API.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -7,27 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace VirtualCharacterSheet.Net.API {
 
-	public sealed class CharacterController {
-		private List<MemoryStream> Streams = new List<MemoryStream>();
+	[ApiController]
+	[Route("[character]")]
+	public sealed class CharacterController : ControllerBase{
 
 		[HttpGet("/character/{identity}")]
 		public HttpResponseMessage Get(string identity) {
+			if(!Data.HasCharacter(identity))
+				return new HttpResponseMessage(HttpStatusCode.NotFound);
 			var result = new HttpResponseMessage(HttpStatusCode.OK);
 			var character = Data.GetCharacter(identity);
 			var stream = new MemoryStream();
 			var writer = new BinaryWriter(stream);
 			PlayerCharacter.Serialize(character, writer);
+			writer.Flush();
 			stream.Position = 0;
 			result.Content = new StreamContent(stream);
 			result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-			Streams.Add(stream);
 			return result;
-		}
-
-		public void DisposeAll() {
-			foreach(MemoryStream stream in Streams)
-				stream.Close();
-			Streams.Clear();
 		}
 
 	}
