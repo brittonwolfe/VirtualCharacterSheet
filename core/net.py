d91e003a9053d661df5af6f52627a47e4e5e4c45
Britton <britton.wolfe1999@gmail.com>
2020-08-19 20:13:12 -0400

    updated references in core.net

--- core/net.py
+++ core/net.py
@@ -1,7 +1,12 @@
 clr.AddReference('vcs')
 
+from shell import PyCli, add_base
+
+def cmd_host():
+	pass
+
 netshell_dict = add_base({
 	'host': cmd_host
 }, prune = ['net', 'view'])
 
-netshell = PyTui(netshell_dict, shout = False, colon = False)
+netshell = PyCli(netshell_dict, shout = False, colon = False)
